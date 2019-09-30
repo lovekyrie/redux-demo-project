eccc719b4a7f741081a08d48f98cd20f3d85d08a
Duan Yupeng <fengdurant@163.com>
2019-09-30 14:41:18 +0800

    add code

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -1,4 +1,16 @@
-const initialState={
-  articles:[]
+import { ADD_ARTICLE } from "../constants/action-types";
+
+const initialState = {
+  articles: []
+};
+
+function rootReducer(state = initialState, action) {
+  if (action.type === ADD_ARTICLE) {
+    return Object.assign({}, state, {
+      articles: state.articles.concat(action.payload)
+    });
+  }
+  return state;
 }
 
+export default rootReducer;
